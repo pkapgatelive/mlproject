aee372b2ecc878e5beb0597cd240180e4551ae85
pkapate <pkapgate@cdac.in>
2023-03-30 16:35:13 +0530

    the evvirnment stup done, requirment file done

---
+++ setup.py
@@ -0,0 +1,30 @@
+# responsible to create the machine learning project as a package
+
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    """
+    
+    this function will return the list of requirements
+    
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements]
+    if HYPEN_E_DOT in  requirements :
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+        
+
+setup(
+name = 'mlproject',
+version = '0.0.1',
+author= 'alpha_chita',
+author_email='pkapgatelive@gmail.com',
+packages= find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
