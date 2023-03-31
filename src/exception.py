717868c9d615c0ae0471b3875f15df7656e2cc46
pkapgatelive <59923992+pkapgatelive@users.noreply.github.com>
2023-03-31 10:45:49 +0530

    the

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
 
+from src.logger import logging
+
 def error_message_detail(error, error_details:sys):
     _,_,exc_tb = error_details.exc_info()
     
@@ -14,7 +16,7 @@ def error_message_detail(error, error_details:sys):
 
 class CustomeException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_details=error_detail)
 
     def __str__(self):
